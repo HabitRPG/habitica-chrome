fa1e5d5008c52e9a3f3aeb05cae7cc2039fc9bf9
Stan Lindsey <contact@stanlindsey.net>
2013-01-26 14:22:48 +0000

    Added custom interval

--- options.js
+++ options.js
@@ -2,6 +2,13 @@ jQuery('document').ready(function(){
       
   // Restore saved values
   $('#uid').val(localStorage.uid);
+  
+  if(localStorage.interval) {
+    $('#interval').val(localStorage.interval);
+  }else {
+      $('#interval').val("5");
+  }
+  
   if(localStorage.viceDomains) {
     $('#viceDomains').val(localStorage.viceDomains);
   }
@@ -11,12 +18,13 @@ jQuery('document').ready(function(){
   }
   
   
-  
+    
   // Saves options to localStorage.
   jQuery('#habitrpgForm').submit(function(){
     localStorage["uid"] = $('#uid').val();
+	localStorage["interval"] = $('#interval').val();
     localStorage["viceDomains"] = $('#viceDomains').val();
-      localStorage["goodDomains"] = $('#goodDomains').val();
+    localStorage["goodDomains"] = $('#goodDomains').val();
     console.log(localStorage);
     $("#status").html("Options Saved.");
     return false; //don't refresh page
