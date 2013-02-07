0bedde536fe467d0035481c80dc6ed8f9fc42096
Ash Jolliffe <a.jolliffe@outlook.com>
2013-02-07 10:13:45 +0000

    Refreshing fork with changes from master

--- options.js
+++ options.js
@@ -3,6 +3,20 @@ jQuery('document').ready(function(){
   // Restore saved values
   $('#uid').val(localStorage.uid);
   
+  $('#apiToken').val(localStorage.apiToken);
+  
+    if(localStorage.workStart) {
+    $('#workStart').val(localStorage.workStart);
+  }else {
+      $('#workStart').val("0");
+  }
+  
+  if(localStorage.workEnd) {
+    $('#workEnd').val(localStorage.workEnd);
+  }else {
+      $('#workEnd').val("24");
+  }
+  
   if(localStorage.interval) {
     $('#interval').val(localStorage.interval);
   }else {
@@ -16,17 +30,27 @@ jQuery('document').ready(function(){
   if(localStorage.goodDomains) {
     $('#goodDomains').val(localStorage.goodDomains);
   }
-  
-  
     
   // Saves options to localStorage.
   jQuery('#habitrpgForm').submit(function(){
     localStorage["uid"] = $('#uid').val();
+	localStorage["apiToken"] = $('#apiToken').val();
 	localStorage["interval"] = $('#interval').val();
     localStorage["viceDomains"] = $('#viceDomains').val();
     localStorage["goodDomains"] = $('#goodDomains').val();
-    console.log(localStorage);
+
+	var work1 = parseInt($('#workStart').val());
+	var work2 = parseInt($('#workEnd').val());
+
+	if( work1 >= work2 ){
+	document.getElementById("status").style.backgroundColor="red"
+	$("#status").html("Work Hours Error: The first number must be lower than the second");
+	} else {
+	localStorage["workStart"] = $('#workStart').val();
+	localStorage["workEnd"] = $('#workEnd').val();
+	console.log(localStorage);
     $("#status").html("Options Saved.");
+	}
     return false; //don't refresh page
   });
   
